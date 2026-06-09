ab10a339cd635ef3622482bd3d7443f403db3981
Konstantin Zwissler <204953663+phkonx@users.noreply.github.com>
2026-06-09 09:49:47 +0700

    Load JASON to dictionary and updated printed output sentece.

--- rates.py
+++ rates.py
@@ -1,5 +1,8 @@
 import requests
+import json
 
 response = requests.get("https://api.frankfurter.app/latest?from=USD&to=THB")
+data = json.loads(response.text)
+rate = data["rates"]["THB"]
 print("statuscode is: ", response.status_code)
-print("the data is ", response.text)
+print("1 USD buys THB ", rate)
